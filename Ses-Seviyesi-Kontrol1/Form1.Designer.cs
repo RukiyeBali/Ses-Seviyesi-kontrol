dda268a81e8e4e5c45084dd155d38c84569e72b5
Rukiye Balı <145977861+RukiyeBali@users.noreply.github.com>
2023-10-16 13:00:49 +0300

    Add files via upload

---
+++ Ses-Seviyesi-Kontrol1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Ses_Seviyesi_Kontrol1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tBarSes = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSeviyeDeger = new System.Windows.Forms.Label();
+            this.lblSesSeviyesi = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tBarSes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tBarSes
+            // 
+            this.tBarSes.Location = new System.Drawing.Point(77, 12);
+            this.tBarSes.Maximum = 100;
+            this.tBarSes.Name = "tBarSes";
+            this.tBarSes.Size = new System.Drawing.Size(491, 45);
+            this.tBarSes.TabIndex = 0;
+            this.tBarSes.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ses Seviye";
+            // 
+            // lblSeviyeDeger
+            // 
+            this.lblSeviyeDeger.AutoSize = true;
+            this.lblSeviyeDeger.Location = new System.Drawing.Point(594, 34);
+            this.lblSeviyeDeger.Name = "lblSeviyeDeger";
+            this.lblSeviyeDeger.Size = new System.Drawing.Size(39, 13);
+            this.lblSeviyeDeger.TabIndex = 2;
+            this.lblSeviyeDeger.Text = "Seviye";
+            // 
+            // lblSesSeviyesi
+            // 
+            this.lblSesSeviyesi.AutoSize = true;
+            this.lblSesSeviyesi.Location = new System.Drawing.Point(156, 83);
+            this.lblSesSeviyesi.Name = "lblSesSeviyesi";
+            this.lblSesSeviyesi.Size = new System.Drawing.Size(70, 13);
+            this.lblSesSeviyesi.TabIndex = 3;
+            this.lblSesSeviyesi.Text = "Ses Seviyesi:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 247);
+            this.Controls.Add(this.lblSesSeviyesi);
+            this.Controls.Add(this.lblSeviyeDeger);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tBarSes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tBarSes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar tBarSes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSeviyeDeger;
+        private System.Windows.Forms.Label lblSesSeviyesi;
+    }
+}
+
---
+++ Ses-Seviyesi-Kontrol1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ses_Seviyesi_Kontrol1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            int deger = tBarSes.Value;
+            lblSeviyeDeger.Text = deger.ToString();
+
+
+            if (deger >= 0 && deger < 25)
+            {
+                lblSesSeviyesi.Text = "Ses Seviyesi Düşük";
+                this.BackColor = Color.CadetBlue;
+            }
+            else if (deger >= 25 && deger < 70)
+            {
+                lblSesSeviyesi.Text = "Ses Seviyesi Normal";
+                this.BackColor = Color.Green;
+            }
+            else if (deger >= 70 && deger < 90)
+            {
+                lblSesSeviyesi.Text = "Ses Seviyesi Yüksek";
+                this.BackColor = Color.OrangeRed;
+            }
+            else
+            {
+                lblSesSeviyesi.Text = "Ses Seviyesi Çok Yüksek";
+                this.BackColor = Color.Red;
+            }
+
+           
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblSesSeviyesi.Text = "Ses Seviyesi Düşük";
+            this.BackColor = Color.CadetBlue;
+            lblSeviyeDeger.Text = "0";
+            { }
+        }
+
+    }
+}
